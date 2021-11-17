1e84a2cd4fbca21dcfe8e87d7b00364efb912d0c
Jack H <jackhalb@bu.edu>
2021-11-16 22:56:54 -0500

    ble progress
    Reduced the BLE screen, trying to print characteristic to console but still receiving null for the value.

--- components/BLEScreen.js
+++ components/BLEScreen.js
@@ -12,52 +12,111 @@ const manager = new BleManager();
 
 export default function ble_set_up() {
 
-    const reducer = (
-        state: Device[],
-        action: { type: 'ADD_DEVICE'; payload: Device } | { type: 'CLEAR' },
-        ): Device[] => {
-        switch (action.type) {
-            case 'ADD_DEVICE':
-            const { payload: device } = action;
+    // const reducer = (
+    //     state: Device[],
+    //     action: { type: 'ADD_DEVICE'; payload: Device } | { type: 'CLEAR' },
+    //     ): Device[] => {
+    //     switch (action.type) {
+    //         case 'ADD_DEVICE':
+    //         const { payload: device } = action;
 
-            // check if the detected device is not already added to the list
-            if (device && !state.find((dev) => dev.id === device.id)) {
-                return [...state, device];
-            }
-            return state;
-            case 'CLEAR':
-            return [];
-            default:
-            return state;
-        }
-    };
+    //         // check if the detected device is not already added to the list
+    //         if (device && !state.find((dev) => dev.id === device.id)) {
+    //             return [...state, device];
+    //         }
+    //         return state;
+    //         case 'CLEAR':
+    //         return [];
+    //         default:
+    //         return state;
+    //     }
+    // };
 
-    // reducer to store and display detected ble devices
-    const [scannedDevices, dispatch] = useReducer(reducer, []);
+    const [isConnected, setIsConnected] = useState(false);
 
     // state to give the user a feedback about the manager scanning devices
     const [isLoading, setIsLoading] = useState(false);
 
-    const scanDevices = () => {
+    const getServicesAndCharacteristics = (device) => {
+    return new Promise((resolve, reject) => {
+        device.services().then(services => {
+            const characteristics = []
+            //console.log("ashu_1",services)
+            services.forEach((service, i) => {
+                service.characteristics().then(c => {
+                //   console.log("service.characteristics")
+                  
+                    characteristics.push(c)
+                    // console.log(characteristics)
+                    resolve(characteristics)
+                    // if (i === services.length - 1) {
+                    //     const temp = characteristics.reduce(
+                    //         (acc, current) => {
+                    //             return [...acc, ...current]
+                    //         },
+                    //         []
+                    //     )
+                    //     const dialog = temp.find(
+                    //         characteristic =>
+                    //             characteristic.isWritableWithResponse
+                    //     )
+                    //     if (!dialog) {
+                    //         reject('No writable characteristic')
+                    //     }
+                    //     resolve(dialog)
+                    // }
+                  
+                })
+            })
+        })
+    })
+}
+
+    const scanForFlextend = async() => {
 
         // display the Activityindicator
         setIsLoading(true);
 
         // scan devices
-        manager.startDeviceScan(null, null, (error, scannedDevice) => {
+        manager.startDeviceScan(null, null, (error, device) => {
             if (error) {
                 console.warn(error);
+                return;
             }
 
             // if a device is detected add the device to the list by dispatching the action into the reducer
-            if (scannedDevice) {
-                if (scannedDevice.name != null){
-                    dispatch({ type: 'ADD_DEVICE', payload: scannedDevice });
+            if (device) {
+                if (device.name == 'Flextend'){
+                    manager.stopDeviceScan(); // stop scanning for other devices
+                    setIsLoading(false);
+
+                    //begin device connection and data reading
+                    manager.connectToDevice(device.id, {autoConnect:true}).then((device) => {
+                        (async() => {
+                            const services = await device.discoverAllServicesAndCharacteristics();
+                            const characteristic = await getServicesAndCharacteristics(services);
+                            // value = services.readCharacteristicForService(services.serviceUUIDs, characteristic.uuid).value;
+                            // console.log("characteristic value:", value);
+                            console.log(characteristic.value)
+                            // console.log("Discovering all services and characteristics", characteristic.uuid);
+                        })();
+                        setIsConnected(true);
+                        return device.discoverAllServicesAndCharacteristics();
+                    }).then((device) => {
+                        
+                    }).then(() => {
+                        console.log("Listening...");
+                    }).catch((error) => {
+                        alert("Connection error", error.message);
+                    });
+                }
+                else{
+                    console.log("No device named Flextend found.")
                 }
             }
         });
 
-        // stop scanning devices after 300 miliseconds
+        // stop scanning devices after 5000 miliseconds
         setTimeout(() => {
             manager.stopDeviceScan();
         }, 5000);
@@ -66,14 +125,8 @@ export default function ble_set_up() {
     return (
         <View>
             <Text style={styles.title}> Set Up BLE  </Text>
-            <Button title="Scan devices" onPress={scanDevices} />
-            <Button title="Clear devices" onPress={() => dispatch({ type: 'CLEAR' })}/>
-            <FlatList
-                keyExtractor={(item) => item.id}
-                data={scannedDevices}
-                renderItem={({ item }) => <DeviceCard device={item} />}
-                contentContainerStyle={styles.device_content}
-            />
+            <Button title="Scan for Flextend" onPress={scanForFlextend} />
+            
         </View>
     );
 }  
