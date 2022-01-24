a42a7f9851128c4b6b6d96ebfa51a71c7ef04cb1
Jack H <jackhalb@bu.edu>
2022-01-24 00:16:46 -0500

    established connection and data transmission with device
    Simplified BLE setup screen to just include connect and disconnect. On connect app reads characteristic properly and shows correct data. Disconnect has not yet been tested.

--- components/BLEScreen.js
+++ components/BLEScreen.js
@@ -2,7 +2,7 @@
 import React, {useState, useEffect, useReducer} from 'react';
 import { TouchableOpacity, Text, View, Button, Image, FlatList, ActivityIndicator, Alert} from 'react-native';
 import styles from '../styles/HomeStyle';
-import { DeviceCard } from '../BLE_components/DeviceCard';
+// import { DeviceCard } from '../BLE_components/DeviceCard';
 
 // Imports for BLE management 
 import { BleManager, Device } from 'react-native-ble-plx';
@@ -33,54 +33,26 @@ export default function ble_set_up() {
     //         return state;
     //     }
     // };
+    // state to give the user a feedback about the manager scanning devices
+    // const [isLoading, setIsLoading] = useState(false);
 
     const [isConnected, setIsConnected] = useState(false);
 
-    // state to give the user a feedback about the manager scanning devices
-    const [isLoading, setIsLoading] = useState(false);
+    async function disconnectFromFlextend() {
+        if (isConnected)
+        {
+            await device.cancelConnection();
+            setIsConnected(false);
+        }
+    }
 
-    const getServicesAndCharacteristics = (device) => {
-    return new Promise((resolve, reject) => {
-        device.services().then(services => {
-            const characteristics = []
-            //console.log("ashu_1",services)
-            services.forEach((service, i) => {
-                service.characteristics().then(c => {
-                //   console.log("service.characteristics")
-                  
-                    characteristics.push(c)
-                    // console.log(characteristics)
-                    resolve(characteristics)
-                    // if (i === services.length - 1) {
-                    //     const temp = characteristics.reduce(
-                    //         (acc, current) => {
-                    //             return [...acc, ...current]
-                    //         },
-                    //         []
-                    //     )
-                    //     const dialog = temp.find(
-                    //         characteristic =>
-                    //             characteristic.isWritableWithResponse
-                    //     )
-                    //     if (!dialog) {
-                    //         reject('No writable characteristic')
-                    //     }
-                    //     resolve(dialog)
-                    // }
-                  
-                })
-            })
-        })
-    })
-}
-
-    const scanForFlextend = async() => {
+    connectToFlextend = async () => {
 
         // display the Activityindicator
-        setIsLoading(true);
+        // setIsLoading(true);
 
         // scan devices
-        manager.startDeviceScan(null, null, (error, device) => {
+        manager.startDeviceScan(null, null, async (error, device) => {
             if (error) {
                 console.warn(error);
                 return;
@@ -88,38 +60,19 @@ export default function ble_set_up() {
 
             // if a device is detected add the device to the list by dispatching the action into the reducer
             if (device) {
-                if (device.name == 'Flextend'){
-                    manager.stopDeviceScan(); // stop scanning for other devices
-                    setIsLoading(false);
-
-                    //begin device connection and data reading
-                    manager.connectToDevice(device.id, {autoConnect:true}).then((device) => {
-                        (async() => {
-                            services = await device.discoverAllServicesAndCharacteristics();
-                            characteristic = await getServicesAndCharacteristics(services).then(
-                                characteristic => console.log(characteristic.value)
-                            )
-                            let val = device.isConnected().then(console.log(val));
-                            // value = services.readCharacteristicForService(services.serviceUUIDs, characteristic.uuid).value;
-                            // console.log("characteristic value:", value);
-                            // console.log(characteristic.read())
-                            //temp = characteristic.read()
-                            //console.log(temp)
-                            // console.log("Discovering all services and characteristics", characteristic.uuid);
-                        })();
-                        setIsConnected(true);
-                        return device.discoverAllServicesAndCharacteristics();
-                    }).then((device) => {
-                        
-                    }).then(() => {
-                        console.log("Listening...");
-                        let val = services.isConnected().then(console.log(val));
-                    }).catch((error) => {
-                        alert("Connection error", error.message);
-                    });
-                }
-                else{
-                    console.log("No device named Flextend found.")
+                if (device.name == "Flextend"){
+                    manager.stopDeviceScan();
+                    await device.connect();
+                    setIsConnected(true);
+                    const allServicesAndCharacteristics = await device.discoverAllServicesAndCharacteristics();
+                    // get the services only
+                    const discoveredServices = await allServicesAndCharacteristics.services();
+                    const myService = discoveredServices[0]; //isolating this service just for testing
+                    const myCharacteristics = await myService.characteristics();
+                    // const readData = myCharacteristics.read();
+                    const characteristicUUID = myCharacteristics[0].uuid;
+                    const readData =  await myService.readCharacteristic(characteristicUUID);
+                    console.log(readData);
                 }
             }
         });
@@ -127,15 +80,14 @@ export default function ble_set_up() {
         // stop scanning devices after 5000 miliseconds
         setTimeout(() => {
             manager.stopDeviceScan();
-        }, 5000);
+        }, 1000);
     };
                 
     return (
         <View>
             <Text style={styles.title}> Set Up BLE  </Text>
-            <Button title="Scan for Flextend" onPress={scanForFlextend} />
-            {/* <Text>{`Connected: ${device.isConnected()}`}</Text> */}
-            {/* <Button title="Read Characteristics" onPress={readDeviceCharacteristics} /> */}
+            <Button title="Connect to Flextend" onPress={connectToFlextend} />
+            <Button title="Disconnect from Flextend" onPress={disconnectFromFlextend}/>
         </View>
     );
 }  
