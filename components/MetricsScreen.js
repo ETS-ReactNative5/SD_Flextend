49bfbb005e35e115c2f1429f5ef5b47080aaa4d1
Thomas <tscrivanich27@gmail.com>
2022-01-29 15:07:20 -0500

    Implemented Full Fix for Metrics Screen

--- components/MetricsScreen.js
+++ components/MetricsScreen.js
@@ -1,32 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
 import styles from '../styles/MetricStyle';
 
-import firestore from '@react-native-firebase/firestore'
 import auth from '@react-native-firebase/auth'
+import firestore from '@react-native-firebase/firestore';
 
-export default class HomeScreen extends React.Component {
+export default function MetricScreen() {
+    
+    const [flexion, setFlexion] = useState();
+    const [extension, setExtension] = useState();
+    const userID = auth().currentUser.phoneNumber;
 
-    static navigationOptions = {
-        title: 'HomeScreen',
+    const getUser = async () => {
+        try {
+            const documentSnapshot = await firestore()
+                .collection('knee health')
+                .doc(userID)
+                .get();
+            
+            const user_data = documentSnapshot.data();
+            setFlexion(user_data["flexion"]);
+            setExtension(user_data["extension"]);
+        } catch {
+            console.log("Error")
+        }
     };
 
-    render() {
+    useEffect(() => {
+        getUser();
+    }, [])
 
-        firestore().collection('knee health').get().then((querySnapshot => {
-            querySnapshot.forEach(documentSnapshot => {
-                if (documentSnapshot.id == auth().currentUser.phoneNumber) {
-                    console.log(documentSnapshot.data())
-                }
-            })
-        }))
 
-        return (
-            <View>
-                <Text style={styles.title}>Hello ?</Text>
-                <Text style={styles.text}>Your Flexion: ? degrees</Text>
-                <Text style={styles.text}>Your Extension: ? degrees</Text>
-            </View>
-        );
-    }
+    return (
+        <View>
+            <Text style={styles.title}>Hello {userID}</Text>
+            <Text style={styles.info_text}>Here are your most recent results:</Text>
+            <Text style={styles.result_text}>Flexion: {flexion} degrees</Text>
+            <Text style={styles.result_text}>Extension: {extension} degrees</Text>
+        </View>
+    )
 }
--- styles/MetricStyle.js
+++ styles/MetricStyle.js
@@ -3,19 +3,29 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
     title: {
         marginTop: 20,
-        fontSize: 24,
+        fontSize: 28,
         textAlign: 'center',
         paddingLeft: 10,
-        fontFamily: 'times new roman',
+        fontFamily: 'arial',
         fontWeight: 'bold',
         color: 'black'
     },
-    text: {
-        marginTop: 20,
-        fontSize: 18,
+    info_text: {
+        marginTop: 20, 
+        fontSize: 24,
         textAlign: 'center',
         paddingLeft: 10,
-        fontFamily: 'times new roman',
+        fontFamily: 'arial',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    result_text: {
+        marginTop: 20,
+        fontSize: 22,
+        textAlign: 'center',
+        paddingLeft: 10,
+        fontFamily: 'arial',
         color: 'black'
     }
 })
